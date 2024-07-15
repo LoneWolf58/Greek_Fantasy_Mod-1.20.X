74b117328ffb7599100b6f11036ff20a0dc6b82c
LoneWolf58 <97279030+LoneWolf58@users.noreply.github.com>
2024-07-15 21:33:36 +0100

    LightningStrikerSword onServerFix

--- src/main/java/net/marshall/greekfantasy/item/custom/LightningStrikerSword.java
+++ src/main/java/net/marshall/greekfantasy/item/custom/LightningStrikerSword.java
@@ -1,11 +1,15 @@
 package net.marshall.greekfantasy.item.custom;
 
 import net.marshall.greekfantasy.GreekFantasy;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -17,10 +21,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -31,57 +37,68 @@ public class LightningStrikerSword extends SwordItem {
     }
      //sword fires only when it has the right amount of charge
     public static int charge;
+    public static int superCharge;
 
     //adds whither effect and builds charge
+
+
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+        if(!player.level().isClientSide()) {
+            ServerLevel level = ((ServerLevel) player.level());
+            BlockPos position = entity.blockPosition();
 
-        if (player.level().isClientSide) {
-            if (entity.level().dimension() == Level.NETHER && entity instanceof LivingEntity livingEntity) {
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.WITHER, 400, 2), player);
+            charge ++;
 
-                charge++;
-            } else if (entity.level().dimension() == Level.OVERWORLD && entity instanceof LivingEntity livingEntity) {
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.WITHER, 400, 10), player);
-
-                charge++;
-            } else if (entity.level().dimension() == Level.END && entity instanceof LivingEntity livingEntity) {
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.WITHER, 400, 2), player);
-
-                charge++;
+            if(charge > 0) {
+                EntityType.LIGHTNING_BOLT.spawn(level, null, (Player)null, position,
+                        MobSpawnType.TRIGGERED, true, true);
             }
-
-            return super.onLeftClickEntity(stack, player, entity);
         }
 
-        return false;
+        return super.onLeftClickEntity(stack, player, entity);
+
+
     }
 
-
-
-   // it is supposed to summon lightning wherever the player is looking by right-clicking
+    // it is supposed to summon lightning wherever the player is looking by right-clicking
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        Vec3 vec3 = pPlayer.getEyePosition().subtract(this.());
-        if (pPlayer.level().isClientSide) {
-            if (charge > 20) {
-                EntityType.LIGHTNING_BOLT.spawn(pLevel, null, null, blockPlayerIsLookingAt(pLevel, pPlayer, 90), MobSpawnType.TRIGGERED, true, true);
-                GreekFantasy.LOGGER.info("Sword was used");
-                charge = charge - 20;
-            } else {
-                GreekFantasy.LOGGER.info("Not Enough Charge!");
-            }
 
-
-            return super.use(pLevel, pPlayer, pUsedHand);
+if (Screen.hasShiftDown()){
+        if (!pPlayer.level().isClientSide) {
+            ServerLevel level = ((ServerLevel) pPlayer.level());
+            BlockPos playerPosition = pPlayer.blockPosition();
+            EntityType.LIGHTNING_BOLT.spawn(level, null, (Player) null, playerPosition,
+                    MobSpawnType.TRIGGERED, true, true);
         }
-        return null;
     }
+
+            //if (!pPlayer.level().isClientSide()) {
+             //   ServerLevel level = ((ServerLevel) pPlayer.level());
+              //  BlockPos position = blockPlayerIsLookingAt(level, pPlayer, 10);
+             //   BlockPos playerPosition = pPlayer.blockPosition();
+
+                //if (charge >= 1) {
+//                        MobSpawnType.TRIGGERED, true, true);
+                //      charge = charge - 1;
+                //     }
+
+
+
+
+
+        return super.use(pLevel, pPlayer, pUsedHand);
+
+    }
+
+
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
 
        pTooltipComponents.add(Component.literal("Charge: " + charge));
+        pTooltipComponents.add(Component.literal("Super Charge: " + superCharge));
 
        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
