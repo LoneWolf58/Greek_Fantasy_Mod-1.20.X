ca85712eb6332aad6ff28a0fca6a0d9d8597cf2a
LoneWolf58 <97279030+LoneWolf58@users.noreply.github.com>
2024-07-19 11:40:12 +0100

    LightningStrikerSword onServerFix

--- src/main/java/net/marshall/greekfantasy/network/PacketHandler.java
+++ src/main/java/net/marshall/greekfantasy/network/PacketHandler.java
@@ -2,31 +2,43 @@ package net.marshall.greekfantasy.network;
 
 import net.marshall.greekfantasy.GreekFantasy;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
+import java.util.Optional;
+
+@Mod.EventBusSubscriber(modid = GreekFantasy.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class PacketHandler {
-    private static final SimpleChannel INSTANCE = ChannelBuilder.named(
-                    new ResourceLocation(GreekFantasy.MOD_ID, "main"))
-            .serverAcceptedVersions((status, version) -> true)
-            .clientAcceptedVersions((status, version) -> true)
-            .networkProtocolVersion(1)
-            .simpleChannel();
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation(GreekFantasy.MOD_ID, "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    private static int packetId = 0;
 
     public static void register() {
-        INSTANCE.messageBuilder(SKeyPressSpawnEntityPacket.class, NetworkDirection.PLAY_TO_SERVER)
-                .encoder(SKeyPressSpawnEntityPacket::encode)
-                .decoder(SKeyPressSpawnEntityPacket::new)
-                .consumerMainThread(SKeyPressSpawnEntityPacket::handle)
-                .add();
+        int id = 0;
+        INSTANCE.registerMessage(id++,
+                SKeyPressSpawnEntityPacket.class,
+                SKeyPressSpawnEntityPacket::encode,
+                SKeyPressSpawnEntityPacket::decode,
+                SKeyPressSpawnEntityPacket::handle,
+                Optional.of(NetworkDirection.PLAY_TO_SERVER)
+        );
     }
+
     public static void sendToServer(Object msg) {
-        INSTANCE.send(msg, PacketDistributor.SERVER.noArg());
+        INSTANCE.sendToServer(msg);
     }
 
     public static void sendToAllClients(Object msg) {
-        INSTANCE.send(msg, PacketDistributor.ALL.noArg());
+        INSTANCE.send(PacketDistributor.ALL.noArg(), msg);
     }
 }
